ffd562f706c1087948c8abf3160204916169f3c0
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-08-14 23:20:51 -0400

    style up

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,30 @@ import "./App.css";
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [gameCount, setGameCount] = useState(1);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [polkeClickedTracker, setPokeClickedTracker] = [];
 
+  const shufflePokemon = (pokeArray) => {
+    for (let i = pokeArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [pokeArray[i], pokeArray[j]] = [pokeArray[j], pokeArray[i]];
+    }
+    setPokemon(pokeArray);
+  };
+
+  const handleButtonClick = (mon) => {
+    if (polkeClickedTracker.include(mon)) {
+      console.log("fail");
+    }
+  };
   useEffect(() => {
-    console.log("calling useEffect");
     const fetchPokemon = async () => {
       const tempArray = [];
       const promises = [];
       const numbers = new Set();
 
-      while (numbers.size < 15) {
+      while (numbers.size < 20) {
         const randomNum = Math.floor(Math.random() * 500) + 1;
         numbers.add(randomNum);
       }
@@ -22,7 +37,7 @@ function App() {
             return response.json();
           })
           .then((data) => {
-            console.log(data);
+            console.log(data.name);
             tempArray.push(data);
           });
         promises.push(promise);
@@ -32,18 +47,21 @@ function App() {
     };
 
     fetchPokemon();
-    console.log("useEffect end");
   }, [gameCount]);
 
   return (
     <>
       <div className="scores">
-        <div className="currentScore">Current Score: 0</div>
+        <div className="currentScore">Current Score: {currentScore}</div>
         <div className="bestScore">Best Score: 0</div>
       </div>
       <div className="container">
-        {pokemon.map((mon) => (
-          <button key={mon} className="mon">
+        {pokemon.slice(0, 15).map((mon) => (
+          <button
+            onClick={handleButtonClick(mon)}
+            key={mon.name}
+            className="mon"
+          >
             <img key={mon} src={mon.sprites.front_default} />
           </button>
         ))}
